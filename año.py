a10666ca755b90991975fe7f7fd0fd9b3fbf3629
sofializardi10-collab <sofia.lizardi10@gmail.com>
2025-09-19 15:25:50 +0000

    Create año.py

---
+++ año.py
@@ -0,0 +1,7 @@
+#calcular la edad a partir del año de nacimiento
+
+año_actual = 2025
+nacimiento = int(input("¿En que año naciste? "))
+
+edad = año_actual - nacimiento
+print("Tienes", edad, "año")
