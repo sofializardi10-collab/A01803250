69455f00260666d18e679185766909a7cbccbce4
sofializardi10-collab <sofia.lizardi10@gmail.com>
2025-09-19 14:55:41 +0000

    Create adolescente.py A01803250

---
+++ adolescente.py
@@ -0,0 +1,8 @@
+#pregunta su edad y determina si es adolescente
+
+edad = int(input("¿Cuantos años tienes? "))
+
+if edad >=13 and edad <= 19:
+  print("Eres adolescente")
+else:
+  print("No eres adolescente")
