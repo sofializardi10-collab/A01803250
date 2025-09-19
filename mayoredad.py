3c5b4aa1148e37680961d641cb286b6935303b3e
sofializardi10-collab <sofia.lizardi10@gmail.com>
2025-09-19 14:44:33 +0000

    A01803250

---
+++ mayoredad.py
@@ -0,0 +1,7 @@
+#pregunta al usuario su edad y determina si es adulto
+
+edad=int(input)("¿Cuantos años tienes? "))
+if edad >= 18:
+  print("Eres mayor de edad ")
+else:
+  print("No eres mayor de edad ")
